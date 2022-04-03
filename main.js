a36440bc3108218fc76ac3ce7c2244ab5518358b
Thorny128 <86695706+Thorny128@users.noreply.github.com>
2022-04-03 19:00:04 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var firstname="Ranbir";
+var lastname="Kapoor";
+
+var a=10;
+var b=5;
+
+console.log("Hello there. I am " + firstname + " " + lastname)
+console.log("My favorite number is " + (a+b))
+
+function Click(){
+    console.log("I will appear whenever you click the button.")
+}
